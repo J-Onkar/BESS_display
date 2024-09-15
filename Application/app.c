6dd63391b5b3c243731c14efed446c47ffa30525
JadhavOnkar <onkar.jadav@log9materials.com>
2024-09-15 18:05:52 +0530

    Display resetting multiple times

--- Application/app.c
+++ Application/app.c
@@ -66,8 +66,8 @@ static q_member_t queue_arr[QUEUE_LEN];
 static CAN_TxHeaderTypeDef tx_hdr;
 static uint32_t tim1, tim2, tim3, tim4;
 
-udan_14_t msg_14;
-udan_13_t msg_13;
+
+
 
 static inline uint16_t HTONS(uint16_t x) {
 	return ((x << 8) | ((x >> 8) & 0xFF));
@@ -119,6 +119,8 @@ static void tx_hdr_init(void) {
 
 static void update_udan_14(uint8_t data[]) {
 
+	udan_14_t msg_14;
+
 	(void) memcpy(msg_14.gen_data, data, 8);
 
 	update_batt_volt_label(HTONS(msg_14.batt_volt));
@@ -128,6 +130,8 @@ static void update_udan_14(uint8_t data[]) {
 }
 
 static void update_udan_13(uint8_t data[]) {
+
+	udan_13_t msg_13;
 
 	(void) memcpy(msg_13.gen_data, data, 8);
 
@@ -172,7 +176,7 @@ static void read_can_message(void) {
 static void timer_check(void) {
 
 	if ((HAL_GetTick() - tim1) > 5000U) {
-		reset_page1_display();
+		//reset_page1_display();
 		tim1 = HAL_GetTick();
 	}
 
@@ -200,7 +204,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 
 	else if (htim->Instance == TIM2) {
 		update_comm_status_label("Loss of Comm");
-		reset_display();
+		//reset_display();
 		if (htim3.State != HAL_TIM_STATE_READY) {
 			HAL_GPIO_WritePin(Buzzer_pin_GPIO_Port, Buzzer_pin_Pin,
 					GPIO_PIN_RESET);
@@ -261,7 +265,7 @@ void app_init(void) {
 
 void can_init(void) {
 
-	CAN_FilterTypeDef filter1;
+	CAN_FilterTypeDef filter1, filter2;
 
 	filter1.FilterActivation = CAN_FILTER_ENABLE;
 	filter1.FilterBank = 0;
@@ -269,14 +273,28 @@ void can_init(void) {
 	filter1.FilterMode = CAN_FILTERMODE_IDMASK;
 	filter1.FilterScale = CAN_FILTERSCALE_32BIT;
 	filter1.FilterIdHigh = 0x0000;
-	filter1.FilterIdLow = 0x8000;
+	filter1.FilterIdLow = 0x9800;
 	filter1.FilterMaskIdHigh = 0x0000;
-	filter1.FilterMaskIdLow = 0x8000;
+	filter1.FilterMaskIdLow = 0xFFF8;
+
+	filter2.FilterActivation = CAN_FILTER_ENABLE;
+	filter2.FilterBank = 1;
+	filter2.FilterFIFOAssignment = CAN_RX_FIFO0;
+	filter2.FilterMode = CAN_FILTERMODE_IDMASK;
+	filter2.FilterScale = CAN_FILTERSCALE_32BIT;
+	filter2.FilterIdHigh = 0x0000;
+	filter2.FilterIdLow = 0xA000;
+	filter2.FilterMaskIdHigh = 0x0000;
+	filter2.FilterMaskIdLow = 0xFFF8;
 
 
 	if (HAL_CAN_ConfigFilter(&hcan, &filter1) != HAL_OK) {
 		Error_Handler();
 	}
+
+	if (HAL_CAN_ConfigFilter(&hcan, &filter2) != HAL_OK) {
+			Error_Handler();
+		}
 
 	if (HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING)
 			!= HAL_OK) {
